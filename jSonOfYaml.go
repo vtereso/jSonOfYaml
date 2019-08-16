5ebaa46b8bbe00c0f5f00b6d1b808a47262e2854
Vincent-DeSousa-Tereso <Vincent.DeSousa.Tereso@ibm.com>
2019-08-16 11:43:19 -0400

    Initial drop: Yaml to Json printer

---
+++ jSonOfYaml.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/ghodss/yaml"
+	"io/ioutil"
+	"os"
+)
+
+// Takes a single yaml file argument and pretty prints it as json
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Filename was not passed")
+		return
+	}
+	file := os.Args[1]
+	yamlBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	jsonBytes, err := yaml.YAMLToJSON(yamlBytes)
+	if err != nil {
+		fmt.Println("Error converting yaml to json:", err)
+	}
+	var out bytes.Buffer
+	json.Indent(&out, jsonBytes, "", "	")
+	if err != nil {
+		fmt.Println("Error indenting json:", err)
+	}
+	fmt.Println(out.String())
+}
