197b7de5027ecd138fb08611b17fc62414dd53af
Vincent-DeSousa-Tereso <Vincent.DeSousa.Tereso@ibm.com>
2019-08-16 12:07:47 -0400

    Adding output file flag

--- jSonOfYaml.go
+++ jSonOfYaml.go
@@ -3,34 +3,52 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/ghodss/yaml"
 	"io/ioutil"
 	"os"
 )
 
+var (
+	o = flag.String("o", "stdout", "The file to output the json into.")
+)
+
 // Takes a single yaml file argument and pretty prints it as json
 func main() {
-	if len(os.Args) != 2 {
+	flag.Parse()
+	os.Args = flag.Args()
+	if len(os.Args) != 1 {
 		fmt.Println("Filename was not passed")
 		return
 	}
-	file := os.Args[1]
-	yamlBytes, err := ioutil.ReadFile(file)
+	yamlFile := os.Args[0]
+	yamlBytes, err := ioutil.ReadFile(yamlFile)
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println(err)
 		return
 	}
 	jsonBytes, err := yaml.YAMLToJSON(yamlBytes)
 	if err != nil {
-		fmt.Println("Error converting yaml to json:", err)
+		fmt.Println(err)
 		return
 	}
 	var out bytes.Buffer
 	json.Indent(&out, jsonBytes, "", "	")
 	if err != nil {
-		fmt.Println("Error indenting json:", err)
+		fmt.Println(err)
 		return
 	}
-	fmt.Println(out.String())
+
+	var outFile *os.File
+	switch *o {
+	case "stdout":
+		outFile = os.Stdout
+	default:
+		outFile, err = os.OpenFile(*o, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Fprintf(outFile, out.String())
 }
